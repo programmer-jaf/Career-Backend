4f37b999f7c6c1df0fcbde49a4833e39ffd71c55
Programmer-Jaf <programmerjaf@gmail.com>
2025-04-17 20:10:24 +0600

    create express server and add middlewares

---
+++ src/app/app.js
@@ -0,0 +1,65 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import config from "../config/config.js";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+
+// create express server
+const app = express();
+
+// cors configuration
+app.use(
+  cors({
+    credentials: true,
+    origin: config.frontend_domain,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+
+// rate limit configuration
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+
+// helmet configuration
+app.use(helmet());
+
+// cookie parser configuration
+app.use(cookieParser());
+
+// express json configuration
+app.use(
+  express.json({
+    limit: "50mb",
+    extended: true,
+  })
+);
+
+// express urlencoded configuration
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+);
+
+// User.Route
+
+// Company.Route
+
+// Blog.Route
+
+// Contact.Route
+
+// Job.Route
+
+// Application.Route
+
+// globalErrorHandler
+
+export default app;
