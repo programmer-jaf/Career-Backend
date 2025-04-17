5d421d259d10b0ed10a4a9f277b22f3603e1c1cf
Programmer-Jaf <programmerjaf@gmail.com>
2025-04-17 21:02:43 +0600

    add global error handler for centralized error management

---
+++ src/global/globalErrorHandler.js
@@ -0,0 +1,25 @@
+import config from "../config/config.js";
+const globalErrorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  const response = {
+    success: false,
+    message,
+  };
+  // in development mode
+  if (config.node_env === "development") {
+    response.stack = err.stack;
+    response.error = err;
+  }
+  // in production mode
+  if (process.env.NODE_ENV === "production") {
+    // Optionally override generic errors
+    if (err.name === "ValidationError") {
+      response.message = "Invalid data provided.";
+    }
+  }
+  res.status(statusCode).json(response);
+};
+
+export default globalErrorHandler;
