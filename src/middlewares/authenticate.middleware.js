146eb36be625b1d7f738643e46bdb08e56702637
Programmer-Jaf <programmerjaf@gmail.com>
2025-04-18 09:58:34 +0600

    add authentication middleware to logout route

---
+++ src/middlewares/authenticate.middleware.js
@@ -0,0 +1,25 @@
+import config from "../config/config.js";
+import jwt from "jsonwebtoken";
+const authenticate = (req, res, next) => {
+  const token = req.cookies.refreshToken;
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
+  const decoded = jwt.verify(token, config.refresh_token_secret);
+
+  if (!decoded) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
+  console.log(decoded)
+  req.user = decoded._id;
+  next();
+};
+
+export default authenticate;
--- src/router/user.routes.js
+++ src/router/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { login, logout, register } from "../controller/user.controller.js";
+import authenticate from "../middlewares/authenticate.middleware.js";
 
 const userRouter = Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
-userRouter.post("/logout", logout);
+userRouter.post("/logout", authenticate, logout);
 export default userRouter;
